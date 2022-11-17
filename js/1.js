eb04e2d59071bf4caef724388b3c08423fb5a25f
iamsimate <u201916521@hust.edu.cn>
2022-11-17 18:14:53 +0800

    Update Hexo Static Content

--- js/1.js
+++ js/1.js
@@ -1,1 +1,29 @@
 <script type="text/javascript" src="//cdn.datacamp.com/dcl-react.js.gz"></script>
+
+<div data-datacamp-exercise data-lang="r">
+	<code data-type="pre-exercise-code">
+		# 这部分代码是在初始化的时候自动运行的
+		b = 6
+	</code>
+	<code data-type="sample-code">
+		# 这里是示例代码
+
+
+		# Print out a
+
+
+	</code>
+	<code data-type="solution">
+		# 这里是答案
+		a <- 5
+
+		# Print out a
+		print(a)
+	</code>
+	<code data-type="sct">
+		test_object("a")
+		test_function("print")
+		success_msg("Great job!")
+	</code>
+	<div data-type="hint">Use the assignment operator (code<-</code>) to create the variable codea</code>.</div>
+</div>
